a01bf5e9e304626c9321fb22c1bf4708a2ee898a
Emre Önsür <emreonsur@gmail.com>
2025-06-28 19:54:24 +0200

    Initialize Spring Boot project with base dependencies and structure

---
+++ smesales/smesales/src/main/java/com/emreonsur/smesales/SmesalesApplication.java
@@ -0,0 +1,13 @@
+package com.emreonsur.smesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmesalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmesalesApplication.class, args);
+	}
+
+}
---
+++ smesales/smesales/src/test/java/com/emreonsur/smesales/SmesalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emreonsur.smesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmesalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
